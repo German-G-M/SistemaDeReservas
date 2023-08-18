7c8396f0b2931f96c1be9c7dd02c483c13f2cf18
German-G-M <91800270+German-G-M@users.noreply.github.com>
2023-08-17 21:46:56 -0400

    Update bdreservas.sql

--- bdreservas.sql
+++ bdreservas.sql
@@ -9,10 +9,10 @@ create database bd_reservas_registros
 /*
 Orden de la creación de las tablas 
 (primero los que no tienen foreign keys)
-- persona
-- rol
-- agencia
-- puerto
+- rol (listo)
+- persona (listo)
+- agencia (listo)
+- puerto (listo)
 - habitacion
 (luego los que tienen foreign keys)
 - usuario
@@ -22,4 +22,67 @@ Orden de la creación de las tablas
 - hospedaje
 - viajelancha
 - alimentacion
+*/
+  
+create table rol (
+  id int primary key,
+  descripcion varchar (50)
+)
+/*
+  roles:
+1) SuperAdministrador
+2) Administrador
+3) Segundo Administrador
+4) Recepcionista
+5) Personal de Alimentos
+6) Personal de Limpieza
+7) Personal de Mantenimiento
+8) Responsable de la reserva
+9) Guia
+*/
+  
+create table persona (
+  id serial primary key,
+  ci varchar(12),
+  nombre varchar(50),
+  nombre2 varchar(50),
+  apellido1 varchar(50),
+  apellido2 varchar(50),
+  telefono1 varchar(15),
+  telefono2 varchar(15),
+  correo varchar(50),
+  direccion varchar(50),
+  observaciones(200)
+)
+  
+create table agencia (
+  id serial primary key,
+  nombre varchar (50),
+  telefono varchar (15),
+  direccion varchar (50),
+  observacion varchar (200)
+)
+
+/*
+tipo agencia:
+1) Agencia de  viajes
+2) Operadora
+3) Particular
+4) Otro
+*/
+
+create table puerto (
+  id serial primary key,
+  codigo varchar (5),
+  descripcion varchar (50)
+)
+
+/*
+código puerto:
+CPC (Copacabana)
+ISS (Isla del sol sur- Yumani)
+ISN (Isla del sol norte-Challapata)
+ISC (Isla del sol centro-Challa)
+ILN (Isla de la luna)
+ISSK (Hotel Puma Punku)
 */
